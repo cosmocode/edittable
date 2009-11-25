1f0484c99d5d0d60e71d234e2b232e8a661dc7cb
Adrian Lang <lang@cosmocode.de>
2009-11-25 14:36:42 +0100

    Handles get eight NBSPs as innerHTML.

--- script.js
+++ script.js
@@ -910,7 +910,7 @@ addInitEvent(function () {
     // Add handles to rows and columns.
     function addHandle(text, before) {
         var handle = document.createElement('TD');
-        handle.innerHTML = text + 'handle';
+        handle.innerHTML = new Array(8 + 1).join('&nbsp;');
         handle.className = 'handle ' + text + 'handle';
         drag.attach(handle);
         this.insertBefore(handle, before);
