048660db082ecd57fb697b5a6e04fb2814f4d4fa
Andreas Gohr <gohr@cosmocode.de>
2009-11-25 15:55:14 +0100

    fixed language file

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,14 @@
 <?php
 
-$lang = array('toggle_header'    => 'Toggle header state',
-              'val_align_left'   => 'Left-align cell',
-              'val_align_center' => 'Center cell',
-              'val_align_right'  => 'Right-align cell',
-              'span_col_plus'    => 'Increase colspan',
-              'span_col_minus'   => 'Reduce colspan',
-              'span_row_plus'    => 'Increase rowspan',
-              'span_row_minus'   => 'Reduce rowspan',
-              'struct_col_plus'  => 'Add column',
-              'struct_col_minus' => 'Remove column',
-              'struct_row_plus'  => 'Add row',
-              'struct_row_minus' => 'Remove row');
+$lang['toggle_header']    = 'Toggle header state';
+$lang['val_align_left']   = 'Left-align cell';
+$lang['val_align_center'] = 'Center cell';
+$lang['val_align_right']  = 'Right-align cell';
+$lang['span_col_plus']    = 'Increase colspan';
+$lang['span_col_minus']   = 'Reduce colspan';
+$lang['span_row_plus']    = 'Increase rowspan';
+$lang['span_row_minus']   = 'Reduce rowspan';
+$lang['struct_col_plus']  = 'Add column';
+$lang['struct_col_minus'] = 'Remove column';
+$lang['struct_row_plus']  = 'Add row';
+$lang['struct_row_minus'] = 'Remove row';
