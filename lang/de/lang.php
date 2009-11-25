586ae7fa0643701aae084b3754485aa202b570de
Andreas Gohr <gohr@cosmocode.de>
2009-11-25 16:49:53 +0100

    First attempt on a German translation
    some strings missing

---
+++ lang/de/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+//$lang['toggle_header']    = 'Toggle header state';
+$lang['val_align_left']   = 'Zelle links ausrichten';
+$lang['val_align_center'] = 'Zelle mitting ausrichten';
+$lang['val_align_right']  = 'Zelle rechts ausrichten';
+//$lang['span_col_plus']    = 'Increase colspan';
+//$lang['span_col_minus']   = 'Reduce colspan';
+//$lang['span_row_plus']    = 'Increase rowspan';
+//$lang['span_row_minus']   = 'Reduce rowspan';
+$lang['struct_col_plus']  = 'Spalte links hinzufügen';
+$lang['struct_col_minus'] = 'Spalte entfernen';
+$lang['struct_row_plus']  = 'Zeile darunter einfügen';
+$lang['struct_row_minus'] = 'Zeile entfernen';
