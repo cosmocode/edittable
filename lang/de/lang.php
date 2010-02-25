b5be1e34c2588e0e5e83bae67b45ef0445646901
Dominik Eckelmann <eckelmann@cosmocode.de>
2010-02-25 15:10:57 +0100

    german translation

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,13 +1,13 @@
 <?php
 
-//$lang['toggle_header']    = 'Toggle header state';
+$lang['toggle_header']    = 'Von Überschrift zu Text bzw von Text zu Überschrift schalten';
 $lang['val_align_left']   = 'Zelle links ausrichten';
 $lang['val_align_center'] = 'Zelle mitting ausrichten';
 $lang['val_align_right']  = 'Zelle rechts ausrichten';
-//$lang['span_col_plus']    = 'Increase colspan';
-//$lang['span_col_minus']   = 'Reduce colspan';
-//$lang['span_row_plus']    = 'Increase rowspan';
-//$lang['span_row_minus']   = 'Reduce rowspan';
+$lang['span_col_plus']    = 'Zeile auf rechte Zelle erweitern';
+$lang['span_col_minus']   = 'Zeile um rechte Zelle verkleinern';
+$lang['span_row_plus']    = 'Spalte um untere Zelle erweitern';
+$lang['span_row_minus']   = 'Unterste Zelle aus Spalte verkleinern';
 $lang['struct_col_plus']  = 'Spalte links hinzufügen';
 $lang['struct_col_minus'] = 'Spalte entfernen';
 $lang['struct_row_plus']  = 'Zeile darunter einfügen';
