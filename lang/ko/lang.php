b0c5e6db0b4d203992379308d6fc751f635d214f
Myeongjin <aranet100@gmail.com>
2013-12-09 15:32:54 +0900

    Add Korean translation

---
+++ lang/ko/lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['toggle_header']    = '머리글 상태 토글';
+$lang['val_align_left']   = '셀 왼쪽 정렬';
+$lang['val_align_center'] = '셀 가운데 정렬';
+$lang['val_align_right']  = '셀 오른쪽 정렬';
+$lang['span_col_plus']    = '열 너비 늘리기';
+$lang['span_col_minus']   = '열 너비 줄이기';
+$lang['span_row_plus']    = '행 높이 늘리기';
+$lang['span_row_minus']   = '행 높이 줄이기';
+$lang['secedit_name']     = '표';
+$lang['js']['confirmdeleterow'] = '행을 삭제하겠습니까?';
+$lang['js']['confirmdeletecol'] = '열을 삭제하겠습니까?';
+$lang['js']['struct_col_plus']  = '오른쪽에 열 추가';
+$lang['js']['struct_col_minus'] = '열 제거';
+$lang['js']['struct_col_plus_before']  = '왼쪽에 열 추가';
+$lang['js']['struct_row_plus']  = '아래에 행 추가';
+$lang['js']['struct_row_minus'] = '행 제거';
+$lang['js']['struct_row_plus_before']  = '위에 행 추가';
+$lang['js']['last_column'] = '마지막 열을 삭제할 수 없습니다!';
+$lang['js']['last_row'] = '마지막 행을 삭제할 수 없습니다!';
+$lang['add_table'] = '새 표 넣기';
