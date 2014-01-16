ba7f4de064fc692260dee99d74f4648d7949bc2d
Andreas Gohr <gohr@cosmocode.de>
2014-01-16 14:59:37 +0100

    all option in context menu now

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['toggle_header']    = 'Toggle header state';
+
 $lang['val_align_left']   = 'Left-align cell';
 $lang['val_align_center'] = 'Center cell';
 $lang['val_align_right']  = 'Right-align cell';
@@ -9,8 +9,6 @@ $lang['span_col_minus']   = 'Reduce colspan';
 $lang['span_row_plus']    = 'Increase rowspan';
 $lang['span_row_minus']   = 'Reduce rowspan';
 $lang['secedit_name']     = 'Table';
-$lang['js']['confirmdeleterow'] = 'Really delete row?';
-$lang['js']['confirmdeletecol'] = 'Really delete column?';
 $lang['js']['struct_col_plus']  = 'Add column on the right';
 $lang['js']['struct_col_minus'] = 'Remove column';
 $lang['js']['struct_col_plus_before']  = 'Add column on the left';
@@ -21,6 +19,17 @@ $lang['js']['last_column'] = 'You cannot delete the last column!';
 $lang['js']['last_row'] = 'You cannot delete the last row!';
 $lang['add_table'] = 'Insert a new table';
 
+
+
+
+$lang['js']['toggle_header']    = 'Toggle header state';
+$lang['js']['align_left']   = 'Left-align cell';
+$lang['js']['align_center'] = 'Center cell';
+$lang['js']['align_right']  = 'Right-align cell';
+
+
+$lang['js']['confirmdeleterow'] = 'Really delete row?';
+$lang['js']['confirmdeletecol'] = 'Really delete column?';
 
 $lang['js']['row_above']  = 'Add row above';
 $lang['js']['remove_row'] = 'Remove row';
--- script/editor.js
+++ script/editor.js
@@ -308,6 +308,66 @@ jQuery(function () {
          */
         contextMenu: {
             items: {
+                toggle_header: {
+                    name: LANG.plugins.edittable.toggle_header,
+                    callback: function (key, selection) {
+                        var col = selection.start.col();
+                        var row = selection.start.row();
+
+                        if(meta[row][col].tag && meta[row][col].tag === 'th'){
+                            meta[row][col].tag = 'td';
+                        }else{
+                            meta[row][col].tag = 'th';
+                        }
+                        this.render();
+                    }
+                },
+                align_left: {
+                    name: LANG.plugins.edittable.align_left,
+                    callback: function (key, selection) {
+                        var col = selection.start.col();
+                        var row = selection.start.row();
+                        meta[row][col].align = 'left';
+                        this.render();
+                    },
+                    disabled: function() {
+                        var selection = this.getSelected();
+                        var row = selection[0];
+                        var col = selection[1];
+                        return (!meta[row][col].align || meta[row][col].align === 'left');
+                    }
+                },
+                align_center: {
+                    name: LANG.plugins.edittable.align_center,
+                    callback: function (key, selection) {
+                        var col = selection.start.col();
+                        var row = selection.start.row();
+                        meta[row][col].align = 'center';
+                        this.render();
+                    },
+                    disabled: function() {
+                        var selection = this.getSelected();
+                        var row = selection[0];
+                        var col = selection[1];
+                        return (meta[row][col].align && meta[row][col].align === 'center');
+                    }
+                },
+                align_right: {
+                    name: LANG.plugins.edittable.align_right,
+                    callback: function (key, selection) {
+                        var col = selection.start.col();
+                        var row = selection.start.row();
+                        meta[row][col].align = 'right';
+                        this.render();
+                    },
+                    disabled: function() {
+                        var selection = this.getSelected();
+                        var row = selection[0];
+                        var col = selection[1];
+                        return (meta[row][col].align && meta[row][col].align === 'right');
+                    }
+                },
+                hsep1: '---------',
                 row_above: {
                     name: LANG.plugins.edittable.row_above
                 },
@@ -325,11 +385,12 @@ jQuery(function () {
                             this.alter("remove_row", selection.start.row(), amount);
                         }
                     }
+                    // fixme don't delete last column
                 },
                 row_below: {
                     name: LANG.plugins.edittable.row_below
                 },
-                hsep1: '---------',
+                hsep2: '---------',
                 col_left: {
                     name: LANG.plugins.edittable.col_left
                 },
@@ -347,11 +408,12 @@ jQuery(function () {
                             this.alter("remove_col", selection.start.col(), amount);
                         }
                     }
+                    // fixme don't delete last row
                 },
                 col_right: {
                     name: LANG.plugins.edittable.col_right
                 },
-                hsep2: '---------',
+                hsep3: '---------',
                 colspan_add: {
                     name: LANG.plugins.edittable.colspan_add,
                     /**
@@ -370,7 +432,7 @@ jQuery(function () {
                             meta[row][col].colspan = 2;
                         }
 
-                        // copy over any data from the merged cell
+                        // copy over any data from the merged cell   fixme handle colspanned cells
                         data[row][col] += ' '+data[row][col+1];
 
                         this.render();
@@ -393,6 +455,34 @@ jQuery(function () {
                         return ((col + span + 1) > end);
                     }
                 },
+                colspan_del: {
+                    name: LANG.plugins.edittable.colspan_del,
+                    /**
+                     * Decrease colspan and rerender table
+                     *
+                     * @param key
+                     * @param selection
+                     */
+                    callback: function(key, selection){
+                        var col = selection.start.col();
+                        var row = selection.start.row();
+
+                        meta[row][col].colspan--;
+                        this.render();
+                    },
+                    /**
+                     * Make available only when colspan is set
+                     *
+                     * @returns {boolean}
+                     */
+                    disabled: function() {
+                        var selection = this.getSelected();
+                        var row = selection[0];
+                        var col = selection[1];
+
+                        return !(meta[row][col].colspan && meta[row][col].colspan > 1);
+                    }
+                },
                 rowspan_add: {
                     name: LANG.plugins.edittable.rowspan_add,
                     /**
@@ -411,7 +501,7 @@ jQuery(function () {
                             meta[row][col].rowspan = 2;
                         }
 
-                        // copy over any data from the merged cell
+                        // copy over any data from the merged cell  fixme handle colspanned cells
                         data[row][col] += ' '+data[row+1][col];
 
                         this.render();
@@ -433,8 +523,35 @@ jQuery(function () {
 
                         return ((row + span + 1) > end);
                     }
+                },
+                rowspan_del: {
+                    name: LANG.plugins.edittable.rowspan_del,
+                    /**
+                     * Decrease colspan and rerender table
+                     *
+                     * @param key
+                     * @param selection
+                     */
+                    callback: function(key, selection){
+                        var col = selection.start.col();
+                        var row = selection.start.row();
+
+                        meta[row][col].rowspan--;
+                        this.render();
+                    },
+                    /**
+                     * Make available only when rowspan is set
+                     *
+                     * @returns {boolean}
+                     */
+                    disabled: function() {
+                        var selection = this.getSelected();
+                        var row = selection[0];
+                        var col = selection[1];
+
+                        return !(meta[row][col].rowspan && meta[row][col].rowspan > 1);
+                    }
                 }
-                //fixme span
 
             }
         }
