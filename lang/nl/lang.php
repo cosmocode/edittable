b5f95815389c66b3c4b3dedd409a8eda57c789c6
Gerrit Uitslag <klapinklapin@gmail.com>
2014-01-27 18:25:30 +0100

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ */
+$lang['secedit_name']          = 'Tabel';
+$lang['add_table']             = 'Een nieuwe tabel invoegen';
+$lang['js']['toggle_header']   = 'Wissel tussen kop en tekst';
+$lang['js']['align_left']      = 'Cel links uitlijnen';
+$lang['js']['align_center']    = 'Cel gecentreerd uitlijnen';
+$lang['js']['align_right']     = 'Cel rechts uitlijnen';
+$lang['js']['confirmdeleterow'] = 'Rij echt verwijderen?';
+$lang['js']['confirmdeletecol'] = 'Kolom echt verwijderen?';
+$lang['js']['row_above']       = 'Rij invoegen hierboven';
+$lang['js']['remove_row']      = 'Rij verwijderen';
+$lang['js']['row_below']       = 'Rij invoegen hieronder';
+$lang['js']['col_left']        = 'Links kolom invoegen';
+$lang['js']['remove_col']      = 'Kolom verwijderen';
+$lang['js']['col_right']       = 'Rechts kolom invoegen';
+$lang['js']['colspan_add']     = 'Kolomsamenvoeging verlengen';
+$lang['js']['colspan_del']     = 'Kolomsamenvoeging verkorten';
+$lang['js']['rowspan_add']     = 'Rijsamenvoeging verlengen';
+$lang['js']['rowspan_del']     = 'Rijsamenvoeging verkorten';
