b235ec4b7bf58a8bd87d6f5a20216422c4a6bbc4
Andreas Gohr <andi@splitbrain.org>
2014-01-29 18:42:21 +0100

    make use of our input wrapper instead of accessing $_POST

--- action/newtable.php
+++ action/newtable.php
@@ -38,43 +38,53 @@ class action_plugin_edittable_newtable extends DokuWiki_Action_Plugin {
      * @param Doku_Event $event
      */
     function handle_newtable($event) {
-        if(!isset($_POST['edittable__new'])) {
-            return;
-        }
-
-        foreach($_POST['edittable__new'] as &$v) {
-            // Form performs a formText
-            $v = cleanText($v);
-        }
-
+        global $INPUT;
         global $TEXT;
-        if(isset($_POST['do']['preview'])) {
-            // preview view of a table edit
-            global $INPUT;
-            $INPUT->set('target', 'table');
-            $_REQUEST['target'] = 'table';
-        } elseif(isset($_POST['do']['edit'])) {
-            // edit view of a table (first edit)
-            $_REQUEST['target'] = 'table';
-            $TEXT               = "^  ^  ^\n";
-            foreach(explode("\n", $_POST['edittable__new']['text']) as $line) {
-                $TEXT .= "| $line |  |\n";
-            }
-        } elseif(isset($_POST['do']['draftdel'])) {
-            $TEXT = $_POST['edittable__new']['pre'].
-                $_POST['edittable__new']['text'].
-                $_POST['edittable__new']['suf'];
-            global $ACT;
-            $ACT                = 'edit';
-            $_REQUEST['target'] = 'section';
-        } elseif(isset($_POST['do']['save'])) {
-            // return to edit page
-            $TEXT = $_POST['edittable__new']['pre'].
-                $TEXT.
-                $_POST['edittable__new']['suf'];
-            global $ACT;
-            $ACT                = 'edit';
-            $_REQUEST['target'] = 'section';
+        global $ACT;
+
+        if(!$INPUT->post->has('edittable__new')) return;
+
+        /*
+         * $fields['pre']  has all data before the selection when the "Insert table" button was clicked
+         * $fields['text'] has all data inside the selection when the "Insert table" button was clicked
+         * $fields['suf']  has all data after the selection when the "Insert table" button was clicked
+         * $TEXT has the table created by the editor (from action_plugin_edittable_editor::handle_table_post())
+         */
+        $fields = $INPUT->post->arr('edittable__new');
+
+        // clean the fields (undos formText()) and update the post and request arrays
+        $fields['pre'] = cleanText($fields['pre']);
+        $fields['text'] = cleanText($fields['text']);
+        $fields['suf'] = cleanText($fields['suf']);
+        $INPUT->post->set('edittable__new', $fields);
+
+
+        $ACT = act_clean($ACT);
+        switch($ACT){
+            case 'preview':
+                // preview view of a table edit
+                $INPUT->post->set('target', 'table');
+                break;
+            case 'edit':
+                // edit view of a table (first edit)
+                $INPUT->post->set('target', 'table');
+                $TEXT = "^  ^  ^\n";
+                foreach(explode("\n", $fields['text']) as $line) {
+                    $TEXT .= "| $line |  |\n";
+                }
+                break;
+            case 'draftdel':
+                // not sure if/how this would happen, we restore all data and hand over to section edit
+                $INPUT->post->set('target', 'section');
+                $TEXT = $fields['pre'].$fields['text'].$fields['suf'];
+                $ACT  = 'edit';
+                break;
+            case 'save':
+                // return to edit page
+                $INPUT->post->set('target', 'section');
+                $TEXT = $fields['pre'].$TEXT.$fields['suf'];
+                $ACT  = 'edit';
+                break;
         }
     }
 }
