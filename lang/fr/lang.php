3edd7b5f6da52216efe581d70e4f5aba9504e95c
Schplurtz le Déboulonné <schplurtz@laposte.net>
2014-01-30 16:45:08 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['secedit_name']          = 'Tableau';
+$lang['add_table']             = 'Insérer une nouvelle ligne.';
+$lang['js']['toggle_header']   = 'dés(activer) entêtes';
+$lang['js']['align_left']      = 'Alignement à gauche';
+$lang['js']['align_center']    = 'Centrage';
+$lang['js']['align_right']     = 'Alignement à droite';
+$lang['js']['confirmdeleterow'] = 'Vraiment supprimer la ligne ?';
+$lang['js']['confirmdeletecol'] = 'vraiment supprimer la colonne ?';
+$lang['js']['row_above']       = 'Ajouter une ligne au dessus';
+$lang['js']['remove_row']      = 'Supprimer la ligne';
+$lang['js']['row_below']       = 'Ajouter une ligne en dessous';
+$lang['js']['col_left']        = 'Ajouter une colonne à gauche';
+$lang['js']['remove_col']      = 'Supprimer la colonne';
+$lang['js']['col_right']       = 'Ajouter une colonne à droite';
+$lang['js']['colspan_add']     = 'Agrandir la cellule horizontalement';
+$lang['js']['colspan_del']     = 'Réduire la cellule horizontalement';
+$lang['js']['rowspan_add']     = 'Augmenter la cellule verticalement';
+$lang['js']['rowspan_del']     = 'Réduire la cellule verticalement';
