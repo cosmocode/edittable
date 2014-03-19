1645236654479369d0b884af2f9c0f21e4088f45
devdho <swdev.dho@gmail.com>
2014-03-19 18:55:39 +0900

    add a korean lang

---
+++ lang/ko/lang.php
@@ -0,0 +1,25 @@
+<?php
+$lang['secedit_name']           = '표';
+$lang['add_table']              = '표 추가';
+
+$lang['js']['toggle_header']    = '머리글 토글';
+$lang['js']['align_left']       = '왼쪽 정렬';
+$lang['js']['align_center']     = '가운데 정렬';
+$lang['js']['align_right']      = '오른쪽 정렬';
+
+$lang['js']['confirmdeleterow'] = '정말 행을 삭제하시겠습니까?';
+$lang['js']['confirmdeletecol'] = '정말 열을 삭제하시겠습니까?';
+
+$lang['js']['row_above']        = '행을 위에 추가';
+$lang['js']['remove_row']       = '행 삭제';
+$lang['js']['row_below']        = '행을 아래에 추가';
+
+$lang['js']['col_left']         = '왼쪽에 열 추가';
+$lang['js']['remove_col']       = '열 삭제';
+$lang['js']['col_right']        = '오른쪽에 열 추가';
+
+$lang['js']['colspan_add']      = '열 병합 확장';
+$lang['js']['colspan_del']      = '열 병합 축소';
+
+$lang['js']['rowspan_add']      = '행 병합 확장';
+$lang['js']['rowspan_del']      = '행 병합 축소';
