5b5d8a22606c7766b3f671327a124c79a5cf9105
Michael Große <grosse@cosmocode.de>
2015-08-18 11:22:19 +0200

    Fix broken toolbar

--- script/handsontable.full.js
+++ script/handsontable.full.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Mon Aug 17 2015 16:30:46 GMT+0200 (CEST)
+ * Date: Tue Aug 18 2015 11:05:44 GMT+0200 (CEST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.16.1',
-  buildDate: 'Mon Aug 17 2015 16:30:46 GMT+0200 (CEST)'
+  buildDate: 'Tue Aug 18 2015 11:05:44 GMT+0200 (CEST)'
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -17360,6 +17360,12 @@ function TableView(instance) {
           return;
         }
         if (next === instance.rootElement) {
+          return;
+        }
+        if ($(next).hasClass('toolbutton')) {
+          return;
+        }
+        if ($(next).hasClass('pickerbutton')) {
           return;
         }
         next = next.parentNode;
