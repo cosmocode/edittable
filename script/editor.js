8f0b81834622a585effa03ed59143f315ba538d2
Michael Grosse <grosse@cosmocode.de>
2016-09-29 16:41:02 +0200

    Add plugin infrastructure
    This replaces the code specific to the tablelayout plugin. That code has
    been moved to said plugin.

--- script/editor.js
+++ script/editor.js
@@ -1,4 +1,5 @@
 var edittable = edittable || {};
+var edittable_plugins = edittable_plugins || {};
 
 (function (edittable) {
     "use strict";
@@ -537,67 +538,16 @@ edittable.loadEditor = function () {
     };
 
 
-    var $layoutfield = $form.find('input[name=tablelayout]');
-    if ($layoutfield.length) {
-        var colWidths = [];
-        var tablelayout = $layoutfield.val();
-        if (tablelayout) {
-            tablelayout = JSON.parse(tablelayout);
-
-
-            tablelayout.colwidth.forEach(function (currentValue, index) {
-                var undefinedValue;
-                if (!currentValue || currentValue.substr(-2) != 'px') {
-                    colWidths.push(undefinedValue);
-                    return;
-                }
-                console.log('Set size of col ' + index + ' to ' + currentValue);
-                colWidths[index] = parseInt(currentValue);
-            });
-        } else {
-            tablelayout = {
-                colwidth: []
-            };
-        }
-        if (colWidths.length) {
-            handsontable_config.manualColumnResize = colWidths;
-        }
-        handsontable_config.afterColumnResize = function (col, width) {
-            if ($layoutfield) {
-                tablelayout.colwidth[col] = width + 'px';
-                $layoutfield.val(JSON.stringify(tablelayout));
+    for (var plugin in edittable_plugins) {
+        if (edittable_plugins.hasOwnProperty(plugin)) {
+            if (typeof edittable_plugins[plugin].modifyHandsontableConfig == "function") {
+                edittable_plugins[plugin].modifyHandsontableConfig(handsontable_config, $form);
             }
-        };
-
-        var forcePreview = false;
-        var originalBeforeRemoveCol = handsontable_config.beforeRemoveCol;
-        handsontable_config.beforeRemoveCol = function (index, amount) {
-            originalBeforeRemoveCol.call(this, index, amount);
-            tablelayout.colwidth.splice(index, amount);
-            $layoutfield.val(JSON.stringify(tablelayout));
-        };
-
-        var originalAfterRemoveCol = handsontable_config.afterRemoveCol;
-        handsontable_config.afterRemoveCol = function (index, amount) {
-            originalAfterRemoveCol.call(this, index, amount);
-            forcePreview = true;
-        };
-
-        var originalAfterCreateCol = handsontable_config.afterCreateCol;
-        handsontable_config.afterCreateCol = function (index, amount) {
-            originalAfterCreateCol.call(this, index, amount);
-            tablelayout.colwidth.splice(index, 0, null);
-            $layoutfield.val(JSON.stringify(tablelayout));
-            forcePreview = true;
-        };
-
-        handsontable_config.afterRender = function () {
-            if (forcePreview) {
-                forcePreview = false;
-                $form.find('button[name="do[preview]"]').click();
-            }
-        };
+        }
     }
+
+
+
 
     $container.handsontable(handsontable_config);
 
