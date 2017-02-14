c14e77e33211dcb33e8af18391c8af14d1400c99
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2017-02-14 06:50:55 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
+ */
+$lang['default colwidth']      = 'Largeur des colonnes. Laisser vider pour une adaptation automatique.';
