0c279a9d6c9ba8436ab044e07ee56eb71fad8989
Marco Hofmann <xenadmin@meinekleinefarm.net>
2017-02-16 22:40:16 +0100

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Marco Hofmann <xenadmin@meinekleinefarm.net>
+ */
+$lang['default colwidth']      = 'Breite der Tabellen spalten. Frei lassen um breite dem Inhalt anzupassen. ';
