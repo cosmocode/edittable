89aa0fdb1e2763ec4fc47ee4facc26d7f3a476bd
anon <anon@anon.com>
2017-02-24 11:30:12 +0100

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -2,8 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ * @author anon <anon@anon.com>
  */
 $lang['secedit_name']          = 'Tabel';
 $lang['add_table']             = 'Een nieuwe tabel invoegen';
@@ -19,7 +20,5 @@ $lang['js']['row_below']       = 'Rij invoegen hieronder';
 $lang['js']['col_left']        = 'Links kolom invoegen';
 $lang['js']['remove_col']      = 'Kolom verwijderen';
 $lang['js']['col_right']       = 'Rechts kolom invoegen';
-$lang['js']['colspan_add']     = 'Kolomsamenvoeging verlengen';
-$lang['js']['colspan_del']     = 'Kolomsamenvoeging verkorten';
-$lang['js']['rowspan_add']     = 'Rijsamenvoeging verlengen';
-$lang['js']['rowspan_del']     = 'Rijsamenvoeging verkorten';
+$lang['js']['merge_cells']     = 'Cellen samenvoegen';
+$lang['js']['unmerge_cells']   = 'Celllen splitsen';
---
+++ lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author anon <anon@anon.com>
+ */
+$lang['default colwidth']      = 'Kolombreedte. Laat leeg om aan te passen aan inhoud.';
