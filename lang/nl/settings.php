01ada2396de4e73498ece2a741fa73df571e6ecf
Klap-in <klapinklapin@gmail.com>
2017-03-03 00:05:14 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Klap-in <klapinklapin@gmail.com>
+ */
+$lang['default colwidth']      = 'Breedte van de kolommen van de tabel. Leeg laten om de breedte op de inhoud te baseren';
