924f09e0016af1fe3aec1585ce28c2a7883128b2
Vojta Olsan <olsh0@seznam.cz>
2017-06-16 17:00:15 +0200

    translation update

---
+++ lang/cs/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Vojta Olsan <olsh0@seznam.cz>
+ */
+$lang['default colwidth']      = 'Šířka sloupců tabulky. Nechte prázné pro šířku podle obsahu.';
