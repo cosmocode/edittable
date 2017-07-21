48b0858105a43ac3ec1901727be7de8e76a3056d
Felix Müller-Donath <j.felix@mueller-donath.de>
2017-07-21 15:00:23 +0200

    translation update

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Felix Müller-Donath <j.felix@mueller-donath.de>
+ */
+$lang['default colwidth']      = 'Breite der Tabellenspalten. Ohne Angabe wird die Breite anhand des Inhaltes ermittelt.';
