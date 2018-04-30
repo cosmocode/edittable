ae14baa94e289fe47ba1f29761e85c09e98aee56
Domingo Redal <docxml@gmail.com>
2018-04-30 17:20:36 +0200

    translation update

---
+++ lang/es/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Domingo Redal <docxml@gmail.com>
+ */
+$lang['secedit_name']          = 'Tabla';
+$lang['add_table']             = 'Insertar una nueva tabla';
+$lang['js']['toggle_header']   = 'Alternar el estado del encabezado';
+$lang['js']['align_left']      = 'Celda alineada a la izquierda';
+$lang['js']['align_center']    = 'Celda centrada';
+$lang['js']['align_right']     = 'Celda alineada a la derecha';
+$lang['js']['confirmdeleterow'] = '¿Realmente desea borrar la fila?';
+$lang['js']['confirmdeletecol'] = '¿Realmente desea borrar la columna?';
+$lang['js']['row_above']       = 'Agregar una fila arriba';
+$lang['js']['remove_row']      = 'Eliminar fila';
+$lang['js']['row_below']       = 'Agregar una fila debajo';
+$lang['js']['col_left']        = 'Añadir columna a la izquierda';
+$lang['js']['remove_col']      = 'Eliminar columna';
+$lang['js']['col_right']       = 'Añadir columna a la derecha';
+$lang['js']['merge_cells']     = 'Combinar celdas';
+$lang['js']['unmerge_cells']   = 'Dividir celdas';
---
+++ lang/es/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Domingo Redal <docxml@gmail.com>
+ */
+$lang['default colwidth']      = 'Ancho de las columnas de la tabla. Dejar vacío para ajustar el ancho según en el contenido';
