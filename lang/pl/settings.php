40bd847f45d96f1245139c7e1e33380b434a628f
Bartek S <sadupl@gmail.com>
2019-10-08 12:00:35 +0200

    translation update

---
+++ lang/pl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['default colwidth']      = 'Szerokość kolumn tabeli. Pozostaw puste do szerokości bazowej.';
