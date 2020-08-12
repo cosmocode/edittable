801fd3a3e9166c8568e20e67bad3cdb07fb97754
Anna Dabrowska <dabrowska@cosmocode.de>
2020-08-12 11:04:26 +0200

    Hogfather-compatible handling of external links without a title
    Fixes #190 and #192

--- renderer/inverse.php
+++ renderer/inverse.php
@@ -25,6 +25,8 @@ class renderer_plugin_edittable_inverse extends Doku_Renderer {
     private $_table = array();
     private $_liststack = array();
     private $quotelvl = 0;
+    private $extlinkparser = null;
+    protected $extlinkPatterns = [];
 
     function getFormat() {
         return 'wiki';
@@ -249,7 +251,7 @@ class renderer_plugin_edittable_inverse extends Doku_Renderer {
         $this->not_block();
         if(strpos($text, '%%') !== false) {
             $this->doc .= "<nowiki>$text</nowiki>";
-        } elseif($text{0} == "\n") {
+        } elseif($text[0] == "\n") {
             $this->doc .= "<nowiki>$text</nowiki>";
         } else {
             $this->doc .= "%%$text%%";
@@ -321,7 +323,7 @@ class renderer_plugin_edittable_inverse extends Doku_Renderer {
         }
         $this->doc .= ">";
         $this->doc .= $text;
-        if($text{0} == "\n") $this->doc .= "\n";
+        if($text[0] == "\n") $this->doc .= "\n";
         $this->doc .= "</$type>";
     }
 
@@ -407,19 +409,62 @@ class renderer_plugin_edittable_inverse extends Doku_Renderer {
     function externallink($url, $name = null) {
         $this->not_block();
 
+        /*
+         * When $name is null it might have been a match of an URL that was in the text without
+         * any link syntax. These are recognized by a bunch of patterns in Doku_Parser_Mode_externallink.
+         * We simply reuse these patterns here. However, since we don't parse the pattern through the Lexer,
+         * no escaping is done on the patterns - this means we need a non-conflicting delimiter. I decided for
+         * a single tick >>'<< which seems to work. Since the patterns contain wordboundaries they are matched
+         * against the URL surrounded by spaces.
+         */
         if($name === null) {
-            // FIXME there is no way of knowing if we are inside link syntax or not
-            $this->doc .= $url;
-            return;
+            // get the patterns from the parser if available, otherwise use a duplicate
+            if(is_null($this->extlinkparser)) {
+                if (
+                    class_exists('\dokuwiki\Parsing\ParserMode\Externallink') &&
+                    method_exists('\dokuwiki\Parsing\ParserMode\Externallink', 'getPatterns')
+                ) {
+                    $this->extlinkparser = new \dokuwiki\Parsing\ParserMode\Externallink();
+                    $this->extlinkparser->preConnect();
+                    $this->extlinkPatterns = $this->extlinkparser->getPatterns();
+                } else {
+                    $ltrs = '\w';
+                    $gunk = '/\#~:.?+=&%@!\-\[\]';
+                    $punc = '.:?\-;,';
+                    $host = $ltrs . $punc;
+                    $any  = $ltrs . $gunk . $punc;
+
+                    $schemes = getSchemes();
+                    foreach ($schemes as $scheme) {
+                        $this->extlinkPatterns[] = '\b(?i)'.$scheme.'(?-i)://['.$any.']+?(?=['.$punc.']*[^'.$any.'])';
+                    }
+
+                    $this->extlinkPatterns[] = '(?<=\s)(?i)www?(?-i)\.['.$host.']+?\.['.$host.']+?['.$any.']+?(?=['.$punc.']*[^'.$any.'])';
+                    $this->extlinkPatterns[] = '(?<=\s)(?i)ftp?(?-i)\.['.$host.']+?\.['.$host.']+?['.$any.']+?(?=['.$punc.']*[^'.$any.'])';
+                }
+            }
+
+            // check if URL matches pattern
+            foreach($this->extlinkPatterns as $pattern) {
+                if(preg_match("'$pattern'", " $url ")) {
+                    $this->doc .= $url; // gotcha!
+                    return;
+                }
+            }
         }
 
+        // still here?
         if($url === "http://$name" || $url === "ftp://$name") {
             // special case - www.* or ftp.* matching
             $this->doc .= $name;
         } else {
+            // link syntax! definitively link syntax
             $this->doc .= "[[$url";
-            $this->doc .= '|';
-            $this->_echoLinkTitle($name);
+            if(!is_null($name)) {
+                // we do have a name!
+                $this->doc .= '|';
+                $this->_echoLinkTitle($name);
+            }
             $this->doc .= ']]';
         }
     }
