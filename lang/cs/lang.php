7bbac100f51ddc7c1e2977814d14e22418e6a3c5
Martin Růžička <martinr@post.cz>
2023-12-08 15:05:45 +0100

    translation update

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -3,22 +3,23 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Martin Růžička <martinr@post.cz>
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  * @author Vojta Olsan <olsh0@seznam.cz>
  */
 $lang['secedit_name']          = 'Tabulka';
 $lang['add_table']             = 'Vložit novou tabulku';
 $lang['js']['toggle_header']   = 'Přepnout stav hlavičky';
-$lang['js']['align_left']      = 'Zarovnat buňku doleva';
-$lang['js']['align_center']    = 'Zarovnat buňku na střed';
-$lang['js']['align_right']     = 'Zarovnat buňku doprava';
-$lang['js']['confirmdeleterow'] = 'Opravdu smazat řádek?';
-$lang['js']['confirmdeletecol'] = 'Opravdu smazat sloupec?';
-$lang['js']['row_above']       = 'Přidat řádek výše';
+$lang['js']['align_left']      = 'Zarovnat text doleva';
+$lang['js']['align_center']    = 'Zarovnat text na střed';
+$lang['js']['align_right']     = 'Zarovnat text doprava';
+$lang['js']['confirmdeleterow'] = 'Určitě chcete smazat řádek?';
+$lang['js']['confirmdeletecol'] = 'Určitě chcete smazat sloupec?';
+$lang['js']['row_above']       = 'Přidat řádek nad';
 $lang['js']['remove_row']      = 'Odstranit řádek';
-$lang['js']['row_below']       = 'Přidat řádek níže';
-$lang['js']['col_left']        = 'Přidat sloupec doleva';
+$lang['js']['row_below']       = 'Přidat řádek pod';
+$lang['js']['col_left']        = 'Přidat sloupec před';
 $lang['js']['remove_col']      = 'Odstranit sloupec';
-$lang['js']['col_right']       = 'Přidat sloupec doprava';
-$lang['js']['merge_cells']     = 'Spojit buňky';
+$lang['js']['col_right']       = 'Přidat sloupec za';
+$lang['js']['merge_cells']     = 'Sloučit buňky';
 $lang['js']['unmerge_cells']   = 'Rozdělit buňky';
--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Martin Růžička <martinr@post.cz>
  * @author Vojta Olsan <olsh0@seznam.cz>
  */
-$lang['default colwidth']      = 'Šířka sloupců tabulky. Nechte prázné pro šířku podle obsahu.';
+$lang['default colwidth']      = 'Šířka sloupců tabulky. Nechte prázdné pro šířku podle obsahu.';
