d1909dfe680024ea150efc840ba17a0b176e8189
Alex Souza <contato@remotoservicos.com.br>
2024-02-10 23:25:40 +0100

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alex Souza <contato@remotoservicos.com.br>
+ */
+$lang['secedit_name']          = 'Tabela';
+$lang['add_table']             = 'Inserir uma nova tabela';
+$lang['js']['toggle_header']   = 'Alternar estado de cabeçalho';
+$lang['js']['align_left']      = 'Alinhar célula à esquerda';
+$lang['js']['align_center']    = 'Centralizar célula';
+$lang['js']['align_right']     = 'Alinhar célula à direita';
+$lang['js']['confirmdeleterow'] = 'Deseja mesmo deletar a linha?';
+$lang['js']['confirmdeletecol'] = 'Deseja mesmo deletar a coluna?';
+$lang['js']['row_above']       = 'Adicionar linha acima';
+$lang['js']['remove_row']      = 'Remover linha';
+$lang['js']['row_below']       = 'Adicionar linha abaixo';
+$lang['js']['col_left']        = 'Adicionar coluna à esquerda';
+$lang['js']['remove_col']      = 'Remover coluna';
+$lang['js']['col_right']       = 'Adicionar coluna à direita';
+$lang['js']['merge_cells']     = 'Mesclar células';
+$lang['js']['unmerge_cells']   = 'Separar células';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alex Souza <contato@remotoservicos.com.br>
+ */
+$lang['default colwidth']      = 'Largura das colunas. Deixe vazio para ajustar ao conteúdo';
