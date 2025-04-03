36d5b3590ec341f13d6b492116222bb7baa04358
Tiberiu Popescu <popescu.tibi@gmail.com>
2025-04-03 15:50:37 +0200

    translation update

---
+++ lang/ro/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tiberiu Popescu <popescu.tibi@gmail.com>
+ */
+$lang['default colwidth']      = 'Lățimea coloanelor. Lasă gol pentru ajustarea în funcție de conținut';
