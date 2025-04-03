63ac3097b7295d2fc2d302ac0f3dfad15d67b276
Tiberiu Popescu <popescu.tibi@gmail.com>
2025-04-03 15:50:37 +0200

    translation update

---
+++ lang/ro/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tiberiu Popescu <popescu.tibi@gmail.com>
+ */
+$lang['secedit_name']          = 'Tabel';
+$lang['add_table']             = 'Inserează un rând nou';
+$lang['js']['toggle_header']   = 'Comută starea cap de coloană';
+$lang['js']['align_left']      = 'Aliniază celula la stânga';
+$lang['js']['align_center']    = 'Centrează celula';
+$lang['js']['align_right']     = 'Aliniază celula la dreapta';
+$lang['js']['confirmdeleterow'] = 'Chiar vrei să ștergi rândul?';
+$lang['js']['confirmdeletecol'] = 'Chiar vrei să ștergi coloana?';
+$lang['js']['row_above']       = 'Adaugă rând nou deasupra';
+$lang['js']['remove_row']      = 'Șterge rândul';
+$lang['js']['row_below']       = 'Adaugă rând nou dedesubt';
+$lang['js']['col_left']        = 'Adaugă coloană la stânga';
+$lang['js']['remove_col']      = 'Șterge coloana';
+$lang['js']['col_right']       = 'Adaugă coloană la dreapta';
+$lang['js']['merge_cells']     = 'Lipește celulele';
+$lang['js']['unmerge_cells']   = 'Desparte celulele';
